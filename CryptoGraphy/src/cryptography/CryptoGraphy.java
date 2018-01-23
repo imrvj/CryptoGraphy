c9dc472b20db769d5dedc0aea07a0921fe771b2b
RV <sng.rv07@gmail.com>
2018-01-23 21:00:37 +0530

    Add files via upload

---
+++ CryptoGraphy/src/cryptography/CryptoGraphy.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cryptography;
+
+/**
+ *
+ * @author Ranvijay
+ */
+public class CryptoGraphy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main m=new Main();
+        m.setVisible(true);
+    }
+    
+}
